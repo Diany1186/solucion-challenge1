c1fe4fcfd821576e3600cb6a8ae3cb1b48191a55
DianyLocal <diany1186@hotmail.com>
2023-01-25 14:09:58 -0500

    implementando función encriptar

--- script.js
+++ script.js
@@ -8,5 +8,21 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
  */
+function btnEncriptar (){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
 
-let matrizCodigo= 
+function encriptar(stringEncriptado){
+    let matrizCodigo= [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptado;
+}    
