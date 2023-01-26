f0b2fa4203bb3fdaac95eaf60eb663dcc7c0f038
DianyLocal <diany1186@hotmail.com>
2023-01-26 14:09:03 -0500

    creando funcionalidad desencriptar

--- script.js
+++ script.js
@@ -25,4 +25,22 @@ function encriptar(stringEncriptado){
         }
     }
     return stringEncriptado;
-}    
+}
+
+function btnDesncriptar (){
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo= [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptado;
+}  
